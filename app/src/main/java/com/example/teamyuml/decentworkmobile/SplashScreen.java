ab8d3d9e8459e8d2065ec80716be79e97a3f8d74
VallenRodJesus <konrad.miskiewicz@makimo.pl>
2018-11-14 12:38:21 +0100

    Coding style corrected

--- app/src/main/java/com/example/teamyuml/decentworkmobile/SplashScreen.java
+++ app/src/main/java/com/example/teamyuml/decentworkmobile/SplashScreen.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/*
+* Start class for Mobile Application
+* created as SplashScreen style
+* */
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        startActivity(new Intent(SplashScreen.this, Login.class));
-        finish();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
+        //created connection with Login Activity
+        startActivity(new Intent(SplashScreen.this, Login.class));
+        finish();
     }
 }
