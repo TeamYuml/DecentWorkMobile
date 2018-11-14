9b3f296fb188241c31de52c5c30e452d8d03d293
VallenRodJesus <konrad.miskiewicz@makimo.pl>
2018-11-14 12:02:45 +0100

    Added styles,manifest reference, intent for StartActivity SplashScreen and icons for future DecentWork App

--- app/src/main/java/com/example/teamyuml/decentworkmobile/SplashScreen.java
+++ app/src/main/java/com/example/teamyuml/decentworkmobile/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.example.teamyuml.decentworkmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +8,11 @@ public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        startActivity(new Intent(SplashScreen.this, Login.class));
+        finish();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
     }
 }
