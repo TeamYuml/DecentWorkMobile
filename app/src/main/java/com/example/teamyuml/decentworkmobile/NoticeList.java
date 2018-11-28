dcd99c3c370ced74c306e14befe8ec819ff6e567
Kamil Supera <kamil.supera@makimo.pl>
2018-11-28 09:32:10 +0100

    Create notice list activity

---
+++ app/src/main/java/com/example/teamyuml/decentworkmobile/NoticeList.java
@@ -0,0 +1,13 @@
+package com.example.teamyuml.decentworkmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoticeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_list);
+    }
+}
