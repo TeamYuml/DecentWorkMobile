eb9ef6c091f42325eb5f78579d4a4e878c43b037
kaniak274 <kamil.kucharski@makimo.pl>
2018-12-13 20:25:28 +0100

    Add drawer layout

--- app/src/main/java/com/example/teamyuml/decentworkmobile/views/NoticeList.java
+++ app/src/main/java/com/example/teamyuml/decentworkmobile/views/NoticeList.java
@@ -4,8 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -31,13 +35,14 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class NoticeList extends AppCompatActivity {
+public class NoticeList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     ArrayList<HashMap<String, String>> noticeAll;
     private ListView noticeList;
     Spinner panelSpinner;
     private static final String NOTICE_URL = VolleyInstance.getBaseUrl() + "/engagments/";
     ImageButton user;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +53,10 @@ public class NoticeList extends AppCompatActivity {
         panelSpinnerAdapter();
         noticeList = findViewById(R.id.noticeList);
         noticeAll = new ArrayList<>();
-        user = findViewById(R.id.user_panel);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void getNotice() {
@@ -140,8 +148,16 @@ public class NoticeList extends AppCompatActivity {
         });
     }
 
-    public void userPanel(View view) {
-        Intent toUserPanel = new Intent(NoticeList.this, UserPanel.class);
-        startActivity(toUserPanel);
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.login:
+                Intent intent = new Intent(this, Login.class);
+                drawerLayout.closeDrawers();
+                startActivity(intent);
+                break;
+        }
+
+        return true;
     }
 }
