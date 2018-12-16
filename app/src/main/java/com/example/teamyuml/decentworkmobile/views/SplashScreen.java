75adce8e49cbbdd6aa19d2c597d09c61effa4656
unknown <n4dregner@gmail.com>
2018-12-16 18:27:55 +0100

    added fonts and stylish code

--- app/src/main/java/com/example/teamyuml/decentworkmobile/views/SplashScreen.java
+++ app/src/main/java/com/example/teamyuml/decentworkmobile/views/SplashScreen.java
@@ -14,7 +14,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashScreen.this, NoticeList.class));
+        startActivity(new Intent(SplashScreen.this, Login.class));
         finish();
     }
 }
