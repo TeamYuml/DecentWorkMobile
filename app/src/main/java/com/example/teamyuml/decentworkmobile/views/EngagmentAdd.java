f7afdf9c0b9fa2499585647db4f3cccad29c018d
KonradMiskiewicz <n4dregner@gmail.com>
2018-12-21 14:41:30 +0100

    stylish code adid changes from advertisment to engagment

--- app/src/main/java/com/example/teamyuml/decentworkmobile/views/EngagmentAdd.java
+++ app/src/main/java/com/example/teamyuml/decentworkmobile/views/EngagmentAdd.java
@@ -33,53 +33,45 @@ import java.util.Map;
  * Parsing data to JSON and sending to the server
  */
 public class EngagmentAdd extends AppCompatActivity implements View.OnClickListener {
-    private EditText advertisment_title_input;
-    private EditText advertisment_description_input;
-
-    private Spinner advertisment_city;
-
-    private Button sign_Advertisment;
-
-    private TextView ErrorTextView;
-
-    List<String> Cities ;
-
+    private EditText engagment_title_input;
+    private EditText engagment_description_input;
+    private Spinner engagment_city;
+    private Button sign_engagment;
+    private TextView errorTextView;
+    List<String> cities ;
     String selected_city;
-
     private static final String Engagment_add_URL = VolleyInstance.getBaseUrl() + "/engagments/engagments/";
-    private static final String TAG = "Create Advertisment";
+    private static final String TAG = "Create engagment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_advertisement_form);
-        advertisment_title_input = findViewById(R.id.Advertisment_Title);
-        advertisment_description_input = findViewById(R.id.Advertiser_Description);
-        advertisment_city = findViewById(R.id.Advertiser_City);
-        ErrorTextView = findViewById(R.id.textView5);
-        Cities = new ArrayList<String>();
-        Cities.add("Wybierz miasto");
-        Cities.add("Łódź");
-        Cities.add("Poznań");
-        Cities.add("Wrocław");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(EngagmentAdd.this,android.R.layout.simple_dropdown_item_1line,Cities);
+        setContentView(R.layout.activity_engagment_form);
+        engagment_title_input = findViewById(R.id.engagment_Title);
+        engagment_description_input = findViewById(R.id.Engagment_Description);
+        engagment_city = findViewById(R.id.Engagment_City);
+        errorTextView = findViewById(R.id.ErrorTextView);
+        cities = new ArrayList<String>();
+        cities.add("Wybierz miasto");
+        cities.add("Łódź");
+        cities.add("Poznań");
+        cities.add("Wrocław");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(EngagmentAdd.this, android.R.layout.simple_dropdown_item_1line, cities);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        advertisment_city.setAdapter(arrayAdapter);
-        advertisment_city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        engagment_city.setAdapter(arrayAdapter);
+        engagment_city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                selected_city = advertisment_city.getItemAtPosition(i).toString();
+                selected_city = engagment_city.getItemAtPosition(i).toString();
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
-        sign_Advertisment = findViewById(R.id.Advertiser_Add);
-        sign_Advertisment.setOnClickListener(this);
+        sign_engagment = findViewById(R.id.Engagment_Add);
+        sign_engagment.setOnClickListener(this);
     }
-
     @Override
-
     /**
      * Makes request to server and add engagment if success or shows
      * error when failure.
@@ -87,31 +79,28 @@ public class EngagmentAdd extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v)
     {
         JSONObject data = this.addParams();
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-        (Request.Method.POST, Engagment_add_URL, data, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+		Request.Method.POST, Engagment_add_URL, data, new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
-
                 try {
                     String owner = response.getString("owner");
-                    ErrorTextView.setText("pomyslnie dodano" + owner);
+                    errorTextView.setText("pomyslnie dodano" + owner);
                 }
                 catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         },  new Response.ErrorListener() {
-
             @Override
               public void onErrorResponse(VolleyError error) {
-                ErrorTextView.setText(error.getMessage());
+                errorTextView.setText(error.getMessage());
                 Log.e(TAG, "Adding Error: " + error.getMessage());
               }
         })
         {
             public Map<String, String> getHeaders() throws AuthFailureError {
-
                 return UserAuth.authorizationHeader(EngagmentAdd.this);
             }
         };
@@ -123,16 +112,14 @@ public class EngagmentAdd extends AppCompatActivity implements View.OnClickListe
      * Method is responsibility for adding params to engagment
      * @return CreateJson object
      */
-    public JSONObject addParams() {
-
-        String advertiser_title = advertisment_title_input.getText().toString();
-        String advertisment_description = advertisment_description_input.getText().toString();
-
+    private JSONObject addParams() {	
+        String engagment_title = engagment_title_input.getText().toString();
+        String engagment_description = engagment_description_input.getText().toString();
 
         CreateJson cj = new CreateJson();
         cj.addStr("city", selected_city);
-        cj.addStr("title" , advertiser_title);
-        cj.addStr("description", advertisment_description);
+        cj.addStr("title" , engagment_title);
+        cj.addStr("description", engagment_description);
 
         try {
             return cj.makeJSON();
@@ -140,6 +127,7 @@ public class EngagmentAdd extends AppCompatActivity implements View.OnClickListe
         catch (JSONException e) {
             e.printStackTrace();
         }
+		
         return null;
     }
 }
